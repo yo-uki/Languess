643312817729aecb8d401607ca822e222bd5c100
Aleksandra <yo-uki@wp.pl>
2025-03-05 20:14:58 +0100

    init

---
+++ src/main/java/pl/youki/Languess/LanguessApplication.java
@@ -0,0 +1,13 @@
+package pl.youki.Languess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/youki/Languess/LanguessApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.youki.Languess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
